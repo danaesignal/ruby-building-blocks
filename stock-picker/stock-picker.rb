0d93f69472f01708bba2d41263d17e98e318988f
David Williams <thedavesignal@gmail.com>
2017-07-14 20:17:11 -0700

    Implements stock picker.

---
+++ stock-picker/stock-picker.rb
@@ -0,0 +1,20 @@
+def stock_picker (pricing_history)
+  @day_offset = 1
+  @best_profit = 0
+  pricing_history.each {|price_today|
+    pricing_history.last(pricing_history.length - @day_offset).each {|future_price|
+      if (future_price - price_today) > @best_profit
+        if (pricing_history.index(future_price) > pricing_history.index(price_today))
+          @best_profit = (future_price - price_today)
+          @buy_date = pricing_history.index(price_today)
+          @sell_date = pricing_history.index(future_price)
+        end
+      end
+    }
+  }
+  puts "Bought on day #{@buy_date+1}, for #{pricing_history[@buy_date]}."
+  puts "Sold on day #{@sell_date+1}, for #{pricing_history[@sell_date]}."
+  puts "Total profit, $#{@best_profit}."
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
